3f0161d4606648b6ace3d160f05fc1d817cfa806
Divit5 <107444717+Divit5@users.noreply.github.com>
2024-01-21 17:13:37 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+Divit = "";
+var c = 3;
+var d = 2;
+var e = 25;
+var f = 25;
+var g = 1;
+var h = 1;
+
+let name = "Divit";
+console.log("My First Console Project" + Divit + "!");
+console.log("Hello Divit Duggal" + Divit + "!");
+let x = 10;
+let y = 5;
+var add = x + y ;
+console.log(add);
+console.log(add);
+var add = c + d ;
+console.log(add);
+var add = e + f ;
+console.log(add);
+var add = g + h ;
+console.log(add);
